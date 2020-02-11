a8e64452020a0edc630ccc1e8ddd18e4cf84d55b
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2020-02-11 14:10:01 -0300

    Add aula-1
    This project contains a simple class with
    main method.
    In the main method, we just print in the output two Strings.

---
+++ aula-1/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,10 @@
+package br.edu.univas.main;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!!!");
+		
+		System.out.println("Seja bem-vindo Rodrigo!");
+	}
+}
