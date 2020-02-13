002ba98768fb352aab07f3d7c47d6b4a45619b24
Rodrigo <rodrigolfsi@gmail.com>
2020-02-13 20:25:02 -0300

    Add aula-2

---
+++ aula-2/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,55 @@
+package br.edu.univas.main;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		//comentario de linha
+		
+		/*
+		  comentário 
+		  de
+		  bloco
+		*/
+		
+		int idade = 10;
+		int ano;
+		System.out.println(idade);
+		System.out.println("idade");
+		
+		byte pequenaVariavel = 11;
+		System.out.println(pequenaVariavel);
+		
+		short outraPequenaVariavel = 12;
+		System.out.println(outraPequenaVariavel);
+		
+		long grandeVariavel = 13;
+		System.out.println(grandeVariavel);
+		
+		float variavelReal = 10.99f;
+		System.out.println(variavelReal);
+		
+		double outraVariavelReal = 10.99;
+		System.out.println(outraVariavelReal);
+		
+		char letter = 'A';
+		System.out.println(letter);
+		
+		String nome = "Rodrigo Luís de Faria";
+		System.out.println(nome);
+		
+		boolean vaiChoverHoje = true;
+		System.out.println(vaiChoverHoje);
+		
+		idade = 15;
+		System.out.println(idade);
+		
+		int soma = idade + 30;
+		System.out.println(idade);
+		System.out.println(soma);
+		
+		int outraSoma = soma + idade;
+		System.out.println(outraSoma);
+		
+	}
+	
+}
